225b9609056cd0da0b2a52b24350cc0f7c6441f9
Kaitlin Yen <109048346+kayenpepper@users.noreply.github.com>
2022-07-28 16:32:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Find classes or IDs */
+// let clue1 = document.querySelector("");
+// let button = document.querySelector("");
+// let clue2 = document.querySelector("");
+
+// let page = document.querySelector(".hero");
+// let body = document.querySelector("#main-body");
+
+// /* Button for the key - Already done! */
+// button.addEventListener("click", (e) => {
+//   button.classList.add("hidden");
+//   clue2.classList.remove("hidden");
+// });
+
+/* Your Code Here */
+window.addEventListener("keypress", (e) => {
+  console.log(e.key);
+})
